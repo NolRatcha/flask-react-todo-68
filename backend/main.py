2eda5548a5b953a84dc0c8a7ed16b31c737e0d23
nal.ratchapol@gmail.com <nal.ratchapol@gmail.com>
2026-01-23 13:59:49 +0700

    Migrated

--- backend/main.py
+++ backend/main.py
@@ -4,18 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 CORS(app)
-
-todo_list = [
-    { "id": 1,
-      "title": 'Learn Flask',
-      "done": True },
-    { "id": 2,
-      "title": 'Build a Flask App',
-      "done": False },
-]
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
 
@@ -23,6 +15,7 @@ class Base(DeclarativeBase):
   pass
 
 db = SQLAlchemy(app, model_class=Base)
+migrate = Migrate(app, db)  
 
 class TodoItem(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -36,8 +29,9 @@ class TodoItem(db.Model):
             "done": self.done
         }
     
-with app.app_context():
-    db.create_all()
+#with app.app_context():
+#    db.create_all()
+
 
 INITIAL_TODOS = [
     TodoItem(title='Learn Flask'),
