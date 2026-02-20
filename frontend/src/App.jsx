f5dc78de5a12f5c89e62bb8905b94fbf6d47f99f
nal.ratchapol@gmail.com <nal.ratchapol@gmail.com>
2026-02-20 15:34:29 +0700

    Final login and logout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from './context/AuthContext.jsx';
 import LoginForm from './LoginForm.jsx';
+import PrivateRoute from "./PrivateRoute.jsx"
 import './App.css'
 
 import TodoList from './TodoList.jsx'
@@ -16,7 +17,9 @@ function App() {
           <Route 
             path="/" 
             element={
-              <TodoList apiUrl={TODOLIST_API_URL}/>
+              <PrivateRoute>                               
+                <TodoList apiUrl={TODOLIST_API_URL}/>
+              </PrivateRoute>
             } 
           />
           <Route
